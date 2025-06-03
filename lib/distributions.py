Fix KL likelihood calculation

lib/distributions.py
@@ -2,6 +2,7 @@ from typing import Tuple
 import jax
 import jax.numpy as jnp
 import jax.random as jnr
+from jax.experimental import checkify
 from jax import Array
 
 MVN_Type = tuple[Array, Array]
@@ -106,9 +107,53 @@ def MVN_multiply(m1: Array, c1: Array, m2: Array, c2: Array) -> tuple[float, MVN
 
     return c, (mean, cov)
 
+def MVN_inverse_bayes(prior: MVN_Type, posterior: MVN_Type):
+    '''
+    Determines the gaussian likelihood function given a gaussian posterior and prior
+
+    Derivation is simple using natural parameters
+
+    Parameter
+    ---------
+    prior: tuple[Array, Array]
+        gaussian prior function
+    posterior: tuple[Array, Array]
+        gaussian posterior function
+    
+    Returns
+    -------
+    tuple[Array, Array]
+        gaussian likelihood function
+    '''
+    # checkify.check(jnp.linalg.det(posterior[1]) < jnp.linalg.det(prior[1]), "Determinant of posterior covariance must be less than prior covariance")
+    # def compare_dets(d):
+    #     d1, d2 = d
+    #     assert d1 < d2, "Determinant of posterior covariance must be less than prior covariance"
+    # jax.experimental.io_callback(compare_dets, (jnp.linalg.det(posterior[1]), jnp.linalg.det(prior[1])))
+    # jax.debug.print("issue: {i}", i=jnp.linalg.det(posterior[1]) > jnp.linalg.det(prior[1]))
+    # \Sigma_l = (\Sigma_posterior^-1 - \Sigma_prior^-1)^-1
+    likelihood_sigma = jnp.linalg.inv(jnp.linalg.inv(posterior[1]) - jnp.linalg.inv(prior[1]))
+    # \mu_l = \Sigma_l @ (\Sigma_posterior^-1 @ \mu_posterior - \Sigma_prior^-1 @ \mu_prior)
+    likelihood_mu = likelihood_sigma @ (jnp.linalg.inv(posterior[1]) @ posterior[0] - jnp.linalg.inv(prior[1]) @ prior[0])
+    return (likelihood_mu, likelihood_sigma)
+
 def MVN_log_likelihood(mean: Array, cov: Array, x: Array) -> float:
     '''
     Calculates the likelihood of the observation under the gaussian distribution
+    
+
+    Parameter
+    ---------
+    mean: Array
+        mean of distribution
+    cov: Array
+        covariance of distribution
+    x: Array
+        observed sample
+    
+    Returns
+    ------
+    float: log likelihood of observing x under the distribution
     '''
     k = mean.shape[-1]
     mean_diff = mean - x
@@ -128,7 +173,7 @@ def MVN_kl_divergence(mu_0: Array, sigma_0: Array, mu_1: Array, sigma_1: Array) 
     c = jnp.log(jnp.linalg.det(sigma_1) / jnp.linalg.det(sigma_0))
     return 0.5 * (a + b - k + c)
 
-def GMM_moment_match(dists: tuple[Array, Array], weights: Array) -> MVN_Type:
+def GMM_moment_match(dists: MVN_Type, weights: Array) -> MVN_Type:
     ''' Finds a gaussian with moments matching a multivariate distribution
 
     Test cases:

test_mot.py
@@ -14,7 +14,7 @@ from dynamax.utils.plotting import plot_uncertainty_ellipses
 from dynamax.linear_gaussian_ssm import LinearGaussianSSM
 from dynamax.linear_gaussian_ssm import lgssm_smoother, lgssm_filter
 from lib.priors import KalmanFilter, KalmanFilter_MOTPDA
-from lib.distributions import MVN_kl_divergence, GMM_moment_match, MVN_multiply, MVN_Type
+from lib.distributions import MVN_kl_divergence, GMM_moment_match, MVN_multiply, MVN_Type, MVN_inverse_bayes
 
 jax.config.update("jax_enable_x64", True)
 # %% [markdown]
@@ -165,10 +165,11 @@ def kf_mot_forward(carry: MVN_Type, x_t: MVN_Type, A: Array, b: Array, Q: Array,
     z_t_given_t = GMM_moment_match(z_t_given_t_s, w_ks)
 
     # Log-Likelihood
-    # project z_{t|t-1} into x (observation) space
+    # project z_{t|t-1} and z_{t|t} into x (observation) space
     z_t_given_t_sub_1_x_space = (H @ z_t_given_t_sub_1[0], H @ z_t_given_t_sub_1[1] @ H.T)
+    z_t_given_t_x_space = (H @ z_t_given_t[0], H @ z_t_given_t[1] @ H.T)
     # get the "effective" observation with moments matching the true GMM
-    x_t_effective = GMM_moment_match(x_t, w_ks)
+    x_t_effective = MVN_inverse_bayes(z_t_given_t_sub_1_x_space, z_t_given_t_x_space)
     # p(x_t) = \int p(z_i|x_{1:i-1}) p(x_i|z_i) dz_i
     log_likelihood = MVN_multiply(*z_t_given_t_sub_1_x_space, *x_t_effective)[0]
 
@@ -177,7 +178,7 @@ def kf_mot_forward(carry: MVN_Type, x_t: MVN_Type, A: Array, b: Array, Q: Array,
 kf_forward = lambda carry, x: kf_mot_forward(carry, x, transition_matrix, jnp.zeros(ndims), transition_noise, jnp.eye(ndims))
 _, result = jax.lax.scan(kf_forward, q_1, zs)
 q_dist, p_dist, log_likelihood = result
-q_dist, p_dist, log_likelihood = KalmanFilter_MOTPDA.run_forward(zs, q_1, transition_matrix, jnp.zeros(ndims), transition_noise, jnp.eye(ndims))
+# q_dist, p_dist, log_likelihood = KalmanFilter_MOTPDA.run_forward(zs, q_1, transition_matrix, jnp.zeros(ndims), transition_noise, jnp.eye(ndims))
 
 ax.scatter(*q_dist[0].T, color="tab:red", label="ours")
 plot_uncertainty_ellipses(*q_dist, ax, **{"edgecolor": "tab:red", "linewidth": 0.5})