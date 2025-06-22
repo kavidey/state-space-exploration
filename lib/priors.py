fix numerical underflow issue and switch to single ball with noisy observations

lib/priors.py
@@ -305,11 +305,15 @@ class KalmanFilter_MOTPDA(KalmanFilter):
     ):
         # find GMM that best represents observations
         z_t_given_t_s, w_ks = jax.vmap(lambda z_t: KalmanFilter_MOTPDA.evaluate_observation(z_t, z_t_given_t_sub_1, H))((x_t[0], x_t[1]))
-        # jax.debug.print("{x} {y}", x=w_ks, y=w_ks / w_ks.sum())
-        # w_ks = jnp.pow(w_ks, 2)
-        # w_ks = w_ks / jax.lax.stop_gradient(w_ks.sum())
-        # w_ks = jnp.ones_like(w_ks)
-        w_ks = jnp.array([1.0, 0.0])
+        # normalize list to fix underflow issues
+        w_ks = w_ks - jnp.max(w_ks) # jax.lax.stop_gradient(jnp.max(w_ks))
+        # sharpen
+        w_ks = w_ks * 100
+        # move out of log space
+        w_ks = jnp.exp(w_ks)
+        
+        w_ks = w_ks / w_ks.sum() # jax.lax.stop_gradient(w_ks.sum())
+        
         # approximate that with a single moment-matched gaussian
         z_t_given_t = GMM_moment_match(z_t_given_t_s, w_ks)
 
@@ -321,6 +325,6 @@ class KalmanFilter_MOTPDA(KalmanFilter):
         
         # This is the same as the log likelihood calculation in KalmanFilter.forward
         z_t_given_t_sub_1_x_space = (H @ z_t_given_t_sub_1[0], H @ z_t_given_t_sub_1[1] @ H.T)
-        w_k = jnp.exp(MVN_multiply(*z_t_given_t_sub_1_x_space, *z_t)[0])
+        w_k = MVN_multiply(*z_t_given_t_sub_1_x_space, *z_t)[0]
 
         return z_t_given_t, w_k

svae_ball.py
@@ -171,14 +171,20 @@ latent_dims = 4
 pos_dims = 2
 num_balls = 2
 
-train = train[:, :, :2]
-test = test[:, :, :2]
+key, tmpkey = jnr.split(key)
+train = jnp.concat((train[:, :, :1], train[:, :, :1] + jnr.normal(tmpkey, train[:, :, :1].shape) * 0.05), axis=2)
+key, tmpkey = jnr.split(key)
+test = jnp.concat((test[:, :, :1], test[:, :, :1] + jnr.normal(tmpkey, test[:, :, :1].shape) * 0.05), axis=2)
 
 train_dataloader = torch.utils.data.DataLoader(torch.tensor(np.asarray(train)), batch_size=batch_size, shuffle=False)
 test_dataloader = torch.utils.data.DataLoader(torch.tensor(np.asarray(test)), batch_size=batch_size, shuffle=False)
 
 process_batch = lambda x: jnp.array(x, dtype='float64')
 setup_batch = process_batch(next(iter(train_dataloader)))
+# %%
+i = 0
+for j in range(num_balls):
+    plt.scatter(setup_batch[i,:,j, -pos_dims], setup_batch[i,:,j, -pos_dims+1])
 # %% Network Definitions
 def initializer_diag_with_noise(epsilon: float):
     '''